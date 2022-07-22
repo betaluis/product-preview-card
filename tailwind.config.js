0bb6e1447ddcfc9866fce4d687428c7e6234289f
betaluis <luis.betancourt.dev@gmail.com>
2022-07-22 17:37:35 +0000

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'body': ['Montserrat', 'sans-serif'],
-        'serif': ['Fraunces', 'serf'],
-      },
+    extend: { 
       fontFamily: { 
         sm: ['10px', '15px'],
         base: ['14px', '20px'],
